fbbfee59cf177d5ab914f3766a798b8697cfd72c
lixiaoqiang <gzhidao1010@gmail.com>
2020-07-23 11:50:29 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GZhiDao1010/library.git master:gh-pages
-
+#git push -f git@github.com:gzhidao1010/library.git master:gh-pages
+git push -f git@github.com:gzhidao1010/library.git master:gh-pages
 cd -
